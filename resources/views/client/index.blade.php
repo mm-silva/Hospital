a81cec911ba7d87903c9619c12de211872aa4fbe
mm-silva <m.meneses@uni9.edu.br>
2020-04-12 10:47:56 -0300

    tela de senha

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,19 +1,66 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<html>
+<head>
+  <style>
+      @media only screen and (max-width: 1073px) {
+      .exac{
+       
+      position:relative;
+      width:100%;
+      margin-left:10px;
+      float:center;
+    }
+}
+    .exac{
+  width:%;margin-left:10px;
 
-<div class="container">
-    <div class="row">
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
-      </div>
-    </div>
+    }
+    .b{
+      border-radius:50px;
+    }
+  
+  
+
+  </style>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+  <div class="container" style="height:100%;padding:5%">
+    <div class="border b">
+      <div class="row">
+          <div class="col-sm-8" height="10%">
+              <h1 class="text-center">One of three columns</h1>
+           <br>
+        <div height="100%" class="col-sm-12 float-left">
+              
+              
+                </div>
+              </div>
+             
+                <div class="row exac">
+          <div class="col-sm-12" style="height:60%">
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4><h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4><h4 class="text-center">One of three columns</h4>
+              <h4 class="text-center">One of three columns</h4>
+             
+          </div>
+      
+            
+                </div>
+  
   </div>
-  <audio id="myAudio">
+</div>
+  {{-- <audio id="myAudio">
   <source src="/sound/sound.ogg" type="audio/ogg">
   <source src="/sound/sound.mp3" type="audio/mpeg">
   Your browser does not support the audio element.
@@ -33,8 +80,11 @@ function playAudio() {
 
 function pauseAudio() { 
   x.pause(); 
-} 
-</script>
+}  --}}
+{{-- </script> --}}
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
